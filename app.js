e99db796d8b3b7857243c84c9bb70103939d6462
antoniopratama <antoniopratama123@gmail.com>
2018-03-15 17:36:22 +0700

    tambah access control allow origin dan headers

--- app.js
+++ app.js
@@ -17,6 +17,16 @@ mongoose.Promise = global.Promise;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authourization");
+    if(req.method === "OPTIONS"){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+})
+
 app.use('/api/users', userRouter);
 app.use('/api/tables', tableRouter);
 app.use('/api/categories', categoryRouter);
